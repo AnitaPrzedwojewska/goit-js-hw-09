ec02b7da85695f0eb1aebb653975b0ed4507c5a2
AnitaPrzedwojewska <144385209+AnitaPrzedwojewska@users.noreply.github.com>
2024-01-22 18:33:02 +0000

    Deploying to gh-pages from @ AnitaPrzedwojewska/goit-js-hw-09@c6bf48b924b1d079502af50e3fcd5ae0a8a0ee43 🚀

--- 01-color-switcher.f713e061.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=null;e.disabled=!1,t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(d)}))}();
-//# sourceMappingURL=01-color-switcher.f713e061.js.map
